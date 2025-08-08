1f407f156e4ce120a121a478d32a21398e2dc21c
Gabriel Vieira <36107057+gb1456v@users.noreply.github.com>
2025-08-08 01:07:12 +0000

    Adiciona novos exercícios em C e configura tarefa de compilação para gcc

---
+++ aula-07.08/Exercicios/exercicio-2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Gabriel ");
+    printf("do ");
+    printf("Carmo ");
+    printf("Vieira ");
+}
---
+++ aula-07.08/Exercicios/exercicio-3.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main() {
+    printf("Gabriel\ndo\nCarmo\nVieira");
+}
