af414c598de97b0bd3a2be2646d12d13cb8feb1d
Gabriel Vieira <36107057+gb1456v@users.noreply.github.com>
2025-08-12 22:33:08 +0000

    Adiciona arquivos de configuração do ambiente de desenvolvimento e novos exercícios 1 a 4.

---
+++ aula-07.08/Exercicios/slide-02/exercicio-3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    float b;
+    printf("Digite um número inteiro e um real:\n");
+    scanf("%d %f", &a, &b);
+    printf("O valor das variaveis a e b, respectivamente, sao: %d, %f \n", a, b);
+}
---
+++ aula-07.08/Exercicios/slide-02/exercicio-4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char palavra[11] = "";
+    printf("Digite uma palavra de até 10 letras:");
+    scanf("%s", palavra);
+    printf("A palavra ")
+
+}
