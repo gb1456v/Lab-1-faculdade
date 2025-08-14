1309fbdec51aa12b4c5304ccce224435058cff04
Gabriel Vieira <36107057+gb1456v@users.noreply.github.com>
2025-08-14 15:20:07 +0000

    Criado exercicio 7

--- aula 12.08/Exercicios/exercicio-7.c
+++ aula 12.08/Exercicios/exercicio-7.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("1.Os alunos são %d. \n", 350);
-    printf("2.Os alunos são %2d. \n", 350);
-    printf("4.Os alunos são %6d. \n", 350);
-    printf("5.Os alunos são %02d. \n", 350);
-    printf("6.Os alunos são %04d. \n", 350);
-    printf("7.Os alunos são %06d. \n", 350);
-    printf("8.Os alunos são %10.02d. \n", 350);
-    printf("9.Os alunos são %10.04d. \n", 350);
-    printf("10.Os alunos são %10.06d. \n", 350);
+    int i=111;
+    printf("*%d*\n", i);
+    printf("*%d*\n", i);
 }
