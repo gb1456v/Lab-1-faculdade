2a221a18e42a4e23a1543b3f0bc1b46406b5abc5
Gabriel Vieira <36107057+gb1456v@users.noreply.github.com>
2025-08-21 15:57:17 +0000

    exercicio 6 finalizado

--- aula 19.08/Exercicios/exercicio-6.c
+++ aula 19.08/Exercicios/exercicio-6.c
@@ -4,10 +4,15 @@ int main() {
     int a,b;
 
     printf("Digite um valor inteiro para a: \n");
-    scanf("%d", &a); // 10
+    scanf("%d", &a);
     printf("Digite um valor inteiro para b: \n");
-    scanf("%d", &b);// 25
-    b= a;
+    scanf("%d", &b);
+    a = a + b;
+    b = a - b; 
+    a = a - b; 
+    printf("a: %d\n", a);
+    printf("b: %d\n", b);
+
     
     
 }
