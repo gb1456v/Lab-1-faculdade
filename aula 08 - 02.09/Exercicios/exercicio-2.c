c860540ff7e9bfde17f2899e4447faf10b25889d
Gabriel Vieira <36107057+gb1456v@users.noreply.github.com>
2025-09-12 19:14:15 +0000

    reorganizada a estrutura das pastas do repositório

--- aula 08 - 02.09/Exercicios/exercicio-2.c
+++ aula 08 - 02.09/Exercicios/exercicio-2.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
-void colidiu_ou_nao() {
+void colidiu_ou_nao()
+{
 
-    int x,y,x_sup,y_sup,x_rec2,y_rec2,x_sup_rec2,y_sup_rec2;
+    int x, y, x_sup, y_sup, x_rec2, y_rec2, x_sup_rec2, y_sup_rec2;
     printf("Retangulo 1\n");
     printf("Digite as coordernadas x0 e y0 do prmeiro retangulo:\n");
-    scanf("%d %d", &x,&y);
+    scanf("%d %d", &x, &y);
     printf("Digite as coordernadas x1 e y1 do prmeiro retangulo:\n");
-    scanf("%d %d", &x_sup,&y_sup);
+    scanf("%d %d", &x_sup, &y_sup);
 
     printf("Retangulo 2\n");
 
     printf("Digite as coordernadas x0 e y0 do prmeiro retangulo:\n");
-    scanf("%d %d", &x_rec2,&y_rec2);
+    scanf("%d %d", &x_rec2, &y_rec2);
     printf("Digite as coordernadas x1 e y1 do prmeiro retangulo:\n");
-    scanf("%d %d", &x_sup_rec2,&y_sup_rec2);
+    scanf("%d %d", &x_sup_rec2, &y_sup_rec2);
 
-    if ((((x_sup - x_sup_rec2) > 0) && ((y_sup - y_sup_rec2) > 0)) || (y_sup_rec2 >)  )  {
+    if ((((x_sup - x_sup_rec2) > 0) && ((y_sup - y_sup_rec2) > 0)) || (y_sup_rec2))
+    {
         printf("1");
     }
 }
-int main() {
+int main()
+{
     colidiu_ou_nao();
 }
-//x0= 0
-//y0= 0
-//x1= 1
-//x0rec2= 1
-//y0rec2= 1
